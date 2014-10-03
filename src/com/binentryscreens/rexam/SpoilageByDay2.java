299406e57142ac5293c03b0f0cbd312c64cccc37
Chris Maher <chrismaher.wit@gmail.com>
2014-10-03 15:37:10 +0100

    Spoilage By Day Refactor.

---
+++ src/com/binentryscreens/rexam/SpoilageByDay2.java
@@ -0,0 +1,30 @@
+
+package com.binentryscreens.rexam;
+
+public class SpoilageByDay2 {
+    
+    public SpoilageByDay2(){
+    
+    
+    }
+    
+    public static void SpoilageByDayGUI(){
+    
+    
+    
+    
+    }
+    
+    public static void checkIfEntryExists(){
+    
+    
+    }
+    
+    
+    
+    public static void setEntryToID(){
+    
+    
+    }
+    
+}
